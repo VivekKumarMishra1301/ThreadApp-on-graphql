400cfbf8a6b617e9d0c60e26902c83f4a925a64a
Vivek <vivekkmishra1301@gmail.com>
2024-04-29 16:10:07 +0530

    created Register user api

--- server/src/graphql/user/resolvers.ts
+++ server/src/graphql/user/resolvers.ts
@@ -1,8 +1,12 @@
+import UserService, { CreateUserPayload } from "../../services/user";
+
 const queries = {};
 
 const mutations = {
-    createUser: async (_:any, {}: {}) => {
-        return "randomid";
+    createUser: async (_:any, payload: CreateUserPayload) => {
+        // return "randomid";
+        const res = await UserService.createUser(payload);
+        return res.id;
     },
 }
 export const resolvers={queries,mutations} 
